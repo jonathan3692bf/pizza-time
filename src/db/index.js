b27a2be3c1be69efa39a6b0970c5f60551a4d849
Jonathan <jonathan3692bf@users.noreply.github.com>
2020-01-22 16:30:01 +0100

    Flesh out remaining routes

--- src/db/index.js
+++ src/db/index.js
@@ -11,8 +11,8 @@ pg.schema.hasTable('orders').then(function (exists) {
     if (!exists) {
         return pg.schema.createTable('orders', function (t) {
             t.increments('id').primary();
-            t.timestamp('createdAt');
-            t.timestamp('lastUpdated');
+            t.timestamp('createdAt').defaultTo(pg.fn.now());
+            t.timestamp('lastUpdated').defaultTo(pg.fn.now());
             t.string('firstName');
             t.string('lastName');
             t.string('streetName');
@@ -21,7 +21,7 @@ pg.schema.hasTable('orders').then(function (exists) {
             t.string('city');
             t.string('phone');
             t.json('items');
-            t.enu('status', ['new', 'preparing', 'delivering', 'delivered'], { useNative: true, enumName: 'order_status_type' })
+            t.enu('status', ['new', 'preparing', 'delivering', 'delivered']).defaultTo('new');
         });
     }
 });
--- src/server.js
+++ src/server.js
@@ -3,24 +3,79 @@ const pg = require('./db/')
 const app = express()
 const port = 3000
 
+const ORDER_TABLE_FIELD_NAMES = ['firstName', 'lastName', 'streetName', 'streetNumber', 'postalCode', 'city', 'phone', 'items'];
+
 app.use(express.json()) // for parsing application/json
 app.use(express.urlencoded({ extended: true })) // for parsing application/x-www-form-urlencoded
 
 
 app.get('/orders', (req, res) => {
-    res.json({method: 'GET', query: req.query})
+    const dbResponse = pg('orders').select();
+
+    dbResponse.then(rows => {
+        res.json({ method: 'GET', query: req.query, rows })
+    })
 })
 app.post('/orders', (req, res) => {
-    res.json(req.body)
+
+    const errors = []
+    const { firstName, lastName, streetName, streetNumber, postalCode, city, phone, items } = req.body
+
+    ORDER_TABLE_FIELD_NAMES.forEach(field => {
+        if ((field === 'items' && req.body.items && req.body.items.length === 0) || (!req.body[field])) {
+            errors.push({
+                field,
+                description: 'Required field'
+            });
+        }
+    })
+    if (errors.length) {
+        return res.json({ errors })
+    }
+
+    pg('orders').insert({
+        status: "new",
+        firstName,
+        lastName,
+        streetName,
+        streetNumber,
+        postalCode,
+        city,
+        phone,
+        items: JSON.stringify(items)
+    }, ['id']).then(row => {
+        res.json({ status: 'success', response: row })
+    }).catch(error => {
+        res.json({ status: 'failure', error })
+    })
+
 })
 app.get('/orders/:orderId', (req, res) => {
-    res.json({ method: 'GET', params: req.params})
+    const dbResponse = pg('orders').where({ 'id': req.params.orderId}).select();
+    dbResponse.then(row => {
+        res.json({ method: 'GET', params: req.params, row })
+    });
 })
 app.put('/orders/:orderId', (req, res) => {
-    res.json({ method: 'PUT', body: req.body, params: req.params })
+    const { status, firstName, lastName, streetName, streetNumber, postalCode, city, phone, items } = req.body
+    const dbResponse = pg('orders').where({ 'id': req.params.orderId }).update({
+        status, firstName, lastName, streetName, streetNumber, postalCode, city, phone, items: JSON.stringify(items)
+    }, ['id', 'status'].concat(ORDER_TABLE_FIELD_NAMES));
+
+    dbResponse.then(row => {
+        res.json({ status: 'success', response: row })
+    }).catch(error => {
+        res.json({ status: 'failure', error, body: req.body})
+    })
 })
 app.delete('/orders/:orderId', (req, res) => {
-    res.json({ method: 'DELETE', params: req.params })
+    const dbResponse = pg('orders').where({ 'id': req.params.orderId }).del();
+    dbResponse.then((rowsDeleted) => {
+        res.json({ status: 'success', orderId: req.params.orderId})//, rowsDeleted })
+    }).catch(error => {
+        res.json({ status: 'failure', error, orderId: req.params.orderId})
+    })
+
 })
 
 app.listen(port, () => console.log(`Express app listening on port ${port}!`))
